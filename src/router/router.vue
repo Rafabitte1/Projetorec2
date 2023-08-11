c361600ddc74e488986f3dec0f6b87b037d1380d
Rafabitte1 <rafabitte1@gmai.com>
2023-08-11 19:40:46 -0300

    incompleto

--- src/router/router.vue
+++ src/router/router.vue
@@ -1,42 +1,24 @@
 <script setup>
-import { ref } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import MeuCarrinho from '@/components/MeuCarrinho.vue';
-import {processarPagamento} from '@/_data/carrinho.js'
-import {
-  processarPagamento,
-
-} from '@/_data/carrinho.js';
-
+import { createRouter, createWebHistory} from "vue-router"
+import homeView from '@/views/homeView.vue'
+import FormView from '@/views/FormView.vue'
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-      {
-        path: '/ListagemLivro',
-        name: 'Livro',
-        component: homeview,
-      },
-      
-      {
-        path: '/carrinho',
-        name: 'carrinho',
-        component: MeuCarrinho
-      },
-      {
-        path: '/processarPagamento',
-        name: 'processaePagamento',
-        component: processarPagamento
-      },
-  
-      {
-        path:'/Formulario',
-        name:'formulario',
-       component: mostrarFormulario
-      },
-    ]
-    
-  },function voltarParaCarrinho() {
-    router.push({ name: 'homeviuew' });
-  })    
-  
-  </script>
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'livraria',
+      component: homeView
+    },
+    {
+      path: '/formulario',
+      name: 'formulario',
+      component: FormView
+    },
+
+  ]
+})
+
+
+
+</script>
